1f833bad29a30b2559ec34f48cd4f400e6dd17bc
Luiz Eduardo <luizebertolini@gmail.com>
2023-02-02 10:28:30 -0300

    Feat (auth): Inclusion of security middleware

--- VIPApi/routes/auth.js
+++ VIPApi/routes/auth.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const authController = require('../controllers/auth/auth')
 const { userService } = require('../services/index')
 const validations = require('../middlewares/validations/schemas/index')
+const permission = require('../middlewares/security/permission')
+
 router.post(
   '/account',
-  [validations.auth.login],
+  [validations.auth.login, permission.auth.login],
   authController.authUser(userService)
 )
 
