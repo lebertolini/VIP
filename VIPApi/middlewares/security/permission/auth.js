ae936f69f60d56cb032a1ed719936ae5cd07c324
Luiz Eduardo <luizebertolini@gmail.com>
2023-02-08 09:09:04 -0300

    Refactor (auth): Inclusion of dependency injection on accout route middleware

--- VIPApi/middlewares/security/permission/auth.js
+++ VIPApi/middlewares/security/permission/auth.js
@@ -1,9 +1,11 @@
-const { getUserByUsername } = require('../../../db/mongodb/methods/userMethods')
+const { userService } = require('../../../services/index')
 const bcrypt = require('bcrypt')
 
 module.exports.login = async (request, response, next) => {
   try {
-    const user = await getUserByUsername(request.body.login.username)
+    const user = await userService.getUserByUsername(
+      request.body.login.username
+    )
     if (
       !user ||
       !bcrypt.compare(request.body.login.password, user.login.password)
